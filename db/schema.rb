f1e9ee84b94669d7016c877a8610500bab54d6c7
prilop <Michael.Prilop@gmail.com>
2013-11-29 15:05:53 +0100

    cleaned up scaffolded stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119151130) do
+ActiveRecord::Schema.define(version: 20131129135032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "app_values", force: true do |t|
-    t.datetime "stamp"
-    t.float    "value"
-    t.integer  "app_id"
-  end
-
-  add_index "app_values", ["app_id"], name: "index_app_values_on_app_id", using: :btree
 
   create_table "apps", force: true do |t|
     t.string   "name"
@@ -30,16 +22,6 @@ ActiveRecord::Schema.define(version: 20131119151130) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "dimension_values", force: true do |t|
-    t.datetime "stamp"
-    t.float    "value"
-    t.integer  "dimension_id"
-    t.integer  "app_value_id"
-  end
-
-  add_index "dimension_values", ["app_value_id"], name: "index_dimension_values_on_app_value_id", using: :btree
-  add_index "dimension_values", ["dimension_id"], name: "index_dimension_values_on_dimension_id", using: :btree
 
   create_table "dimensions", force: true do |t|
     t.string   "name"
@@ -52,16 +34,6 @@ ActiveRecord::Schema.define(version: 20131119151130) do
   end
 
   add_index "dimensions", ["app_id"], name: "index_dimensions_on_app_id", using: :btree
-
-  create_table "metric_values", force: true do |t|
-    t.datetime "stamp"
-    t.float    "value"
-    t.integer  "metric_id"
-    t.integer  "dimension_value_id"
-  end
-
-  add_index "metric_values", ["dimension_value_id"], name: "index_metric_values_on_dimension_value_id", using: :btree
-  add_index "metric_values", ["metric_id"], name: "index_metric_values_on_metric_id", using: :btree
 
   create_table "metrics", force: true do |t|
     t.string   "name"
@@ -93,5 +65,13 @@ ActiveRecord::Schema.define(version: 20131119151130) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "values", force: true do |t|
+    t.datetime "stamp"
+    t.float    "value"
+    t.integer  "metric_id"
+  end
+
+  add_index "values", ["metric_id"], name: "index_values_on_metric_id", using: :btree
 
 end
