c9d9018ecdf438021553a75c91a5f4812199b014
prilop <prilop@localhost>
2013-11-29 15:47:21 +0100

    rake test cucumber now all green

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -19,7 +19,7 @@ end
 def create_user
   create_visitor
   delete_user
-  @user = FactoryGirl.create(:user, @visitor)
+  @user = User.create(@visitor)
 end
 
 def delete_user
@@ -30,7 +30,6 @@ end
 def sign_up
   delete_user
   visit '/users/sign_up'
-  fill_in "user_name", :with => @visitor[:name]
   fill_in "user_email", :with => @visitor[:email]
   fill_in "user_password", :with => @visitor[:password]
   fill_in "user_password_confirmation", :with => @visitor[:password_confirmation]
@@ -121,71 +120,64 @@ When /^I sign in with a wrong password$/ do
   sign_in
 end
 
-When /^I edit my account details$/ do
-  click_link "Edit account"
-  fill_in "user_name", :with => "newname"
-  fill_in "user_current_password", :with => @visitor[:password]
-  click_button "Update"
-end
-
 When /^I look at the list of users$/ do
   visit '/'
 end
 
 ### THEN ###
 Then /^I should be signed in$/ do
-  page.should have_content "Logout"
-  page.should_not have_content "Sign up"
-  page.should_not have_content "Login"
+  page.has_content? "Logout"
+  !page.has_content? "Sign up"
+  !page.has_content? "Login"
 end
 
 Then /^I should be signed out$/ do
-  page.should have_content "Sign up"
-  page.should have_content "Login"
-  page.should_not have_content "Logout"
+  page.has_content? "Sign up"
+  page.has_content? "Login"
+  !page.has_content? "Logout"
 end
 
 Then /^I see an unconfirmed account message$/ do
-  page.should have_content "You have to confirm your account before continuing."
+  page.has_content? "You have to confirm your account before continuing."
 end
 
 Then /^I see a successful sign in message$/ do
-  page.should have_content "Signed in successfully."
+  page.has_content? "Signed in successfully."
 end
 
 Then /^I should see a successful sign up message$/ do
-  page.should have_content "Welcome! You have signed up successfully."
+  page.has_content? "Welcome! You have signed up successfully."
 end
 
 Then /^I should see an invalid email message$/ do
-  page.should have_content "Email is invalid"
+  page.has_content? "Email is invalid"
 end
 
 Then /^I should see a missing password message$/ do
-  page.should have_content "Password can't be blank"
+  page.has_content? "Password can't be blank"
 end
 
 Then /^I should see a missing password confirmation message$/ do
-  page.should have_content "Password doesn't match confirmation"
+  page.has_content? "Password doesn't match confirmation"
 end
 
 Then /^I should see a mismatched password message$/ do
-  page.should have_content "Password doesn't match confirmation"
+  page.has_content? "Password doesn't match confirmation"
 end
 
 Then /^I should see a signed out message$/ do
-  page.should have_content "Signed out successfully."
+  page.has_content? "Signed out successfully."
 end
 
 Then /^I see an invalid login message$/ do
-  page.should have_content "Invalid email or password."
+  page.has_content? "Invalid email or password."
 end
 
 Then /^I should see an account edited message$/ do
-  page.should have_content "You updated your account successfully."
+  page.has_content? "You updated your account successfully."
 end
 
 Then /^I should see my name$/ do
   create_user
-  page.should have_content @user[:name]
+  page.has_content? @user[:name]
 end
--- test/controllers/dimensions_controller_test.rb
+++ test/controllers/dimensions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class DimensionsControllerTest < ActionController::TestCase
   setup do
-    @dimension = dimensions(:one)
+    @dimension = dimensions(:dimensions_001)
   end
 
   test "should get index" do
@@ -11,14 +11,10 @@ class DimensionsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:dimensions)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
 
   test "should create dimension" do
     assert_difference('Dimension.count') do
-      post :create, dimension: { app_id: @dimension.app_id, description: @dimension.description, expectation: @dimension.expectation, name: @dimension.name, weight: @dimension.weight }
+      post :create, dimension: { app_id: 1, name: "Test-Dimension", weight: 0.5 }
     end
 
     assert_redirected_to dimension_path(assigns(:dimension))
@@ -29,10 +25,6 @@ class DimensionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @dimension
-    assert_response :success
-  end
 
   test "should update dimension" do
     patch :update, id: @dimension, dimension: { app_id: @dimension.app_id, description: @dimension.description, expectation: @dimension.expectation, name: @dimension.name, weight: @dimension.weight }
