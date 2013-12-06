ea0a0c452700a2d2c448a2d0b1e3d1e4632ea6fe
prilop <Michael.Prilop@gmail.com>
2013-12-06 15:12:47 +0100

    started implementing logic - values store utc time and metrics return last value

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,20 +1,22 @@
 class Metric < ActiveRecord::Base
   belongs_to :dimension
-  has_many :values
+  has_many :values, -> { order "stamp" }
+
 
   def to_s
     name
   end
 
   def record(value, stamp = Time.now)
-    MetricValue.new(value: value, stamp: stamp)
-    MetricValueRollup.create_or_update(stamp)
-    DimensionValueRollup.create_or_update(stamp)
-    # for each resolution (calculated from stamp) do
-    # rollup_value refresh
-    ## select all last values from resolution(stamp) for dimension
-    ## calculate weighed average, min, max
-    AppValueRollup.create_or_update(stamp)
-    # see before
+    Value.create!(value: value, stamp: stamp, metric: self)
+    #MetricValue.new(value: value, stamp: stamp)
+    #MetricValueRollup.create_or_update(stamp)
+    #DimensionValueRollup.create_or_update(stamp)
+    ## for each resolution (calculated from stamp) do
+    ## rollup_value refresh
+    ### select all last values from resolution(stamp) for dimension
+    ### calculate weighed average, min, max
+    #AppValueRollup.create_or_update(stamp)
+    ## see before
   end
 end
--- test/models/metric_test.rb
+++ test/models/metric_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class MetricTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @metric = metrics(:metrics_001)
+  end
+
+  test "metric should record value and timestamp" do
+    value = 0.38
+    timestamp = Time.now
+    assert @metric.record(value, timestamp)
+    @metric.reload
+    assert @metric.values.last.value.eql?(value)
+    assert @metric.values.last.stamp.to_i == timestamp.to_i
+  end
+
+  test "metric should always return the most recent value" do
+    timestamp = Time.now
+    @metric.record(0.8, timestamp + 5.minutes)
+    @metric.record(0, timestamp)
+    assert @metric.values.last.value.eql?(0.8)
+  end
+
 end
