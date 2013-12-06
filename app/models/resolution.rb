ef29523b0da5de82c8075703a270fe391ef55931
prilop <Michael.Prilop@gmail.com>
2013-12-06 17:02:07 +0100

    Rollups with average, min, max are now calculated for Metric in all Resolutions

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -1,14 +1,15 @@
 class Resolution
 
   def self.defaults
-    [:hour, :day, :month, :year]
+    [:hour, :day, :week, :month, :year]
   end
 
   def self.utc_timestamp_for(resolution, stamp = Time.now)
-    Time.utc(stamp.year, stamp.month, stamp.day, stamp.hour) if resolution.to_sym.eql?(:hour)
-    Time.utc(stamp.year, stamp.month, stamp.day) if resolution.to_sym.eql?(:day)
-    Time.utc(stamp.year, stamp.month) if resolution.to_sym.eql?(:month)
-    Time.utc(stamp.year) if resolution.to_sym.eql?(:year)
+    return Time.utc(stamp.year, stamp.month, stamp.day, stamp.hour) if resolution.to_sym.eql?(:hour)
+    return Time.utc(stamp.year, stamp.month, stamp.day) if resolution.to_sym.eql?(:day)
+    return Time.utc(stamp.year, stamp.month, stamp.day).beginning_of_week if resolution.to_sym.eql?(:week)
+    return Time.utc(stamp.year, stamp.month) if resolution.to_sym.eql?(:month)
+    return Time.utc(stamp.year) if resolution.to_sym.eql?(:year)
 
     raise ResolutionException
   end
--- db/migrate/20131206140937_create_rollups.rb
+++ db/migrate/20131206140937_create_rollups.rb
@@ -2,9 +2,9 @@ class CreateRollups < ActiveRecord::Migration
   def change
     create_table :rollups do |t|
       t.timestamp :stamp
-      t.float :avg
-      t.float :min
-      t.float :max
+      t.float :avg, default: 0.0
+      t.float :min, default: 1.0
+      t.float :max, default: 0.0
       t.string :resolution
       t.references :metric, index: true
       t.references :dimension, index: true
