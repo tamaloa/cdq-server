8822558e29c5c98c7f28e0b540b583c5455fcb96
prilop <prilop@localhost>
2013-12-06 15:12:47 +0100

    started implementing logic - values store utc time and metrics return last value

--- test/models/value_test.rb
+++ test/models/value_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ValueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Value should always store time as UTC" do
+    weird_time = Time.use_zone(ActiveSupport::TimeZone.new('Hawaii')){Time.zone.now}
+    value = Value.create(value: 0.32, stamp: weird_time )
+    value.reload.stamp.eql?(weird_time.utc)
+  end
+
 end
