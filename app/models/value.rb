fadae5a14167f9e9d4af323d8b8c19430ec46158
prilop <Michael.Prilop@gmail.com>
2014-03-06 14:18:40 +0100

    added improvement_advice to value

--- app/models/value.rb
+++ app/models/value.rb
@@ -1,4 +1,11 @@
 class Value < ActiveRecord::Base
   belongs_to :metric
   has_and_belongs_to_many :rollups
+
+  # To optimize performance (avoid loading large text column for other stuff than displaying the text) check out
+  #https://github.com/willbryant/columns_on_demand
+  #https://github.com/choonkeat/binary_column_table
+  # and compare performance.
+  # Note: text columns in postgres are TOASTED (i.e. compressed by default)
+
 end
