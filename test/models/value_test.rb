3fa8c22dd8aba8589837b3731b5b954bc7060e59
prilop <Michael.Prilop@gmail.com>
2014-04-04 16:52:31 +0200

    Make sure Rollups are allways added for Value

--- test/models/value_test.rb
+++ test/models/value_test.rb
@@ -4,14 +4,14 @@ class ValueTest < ActiveSupport::TestCase
 
   test "Value should always store time as UTC" do
     weird_time = Time.use_zone(ActiveSupport::TimeZone.new('Hawaii')){Time.zone.now}
-    value = Value.create(value: 0.32, stamp: weird_time )
+    value = Value.create!(value: 0.32, stamp: weird_time, metric: Metric.first)
     assert_equal weird_time.utc.to_s, value.reload.stamp.to_s
   end
 
   test "Value should store abitrary long improvement advice" do
     advice = "Some long Text! "
     repeat = 10000
-    value = Value.create(value: 0.23, stamp: Time.now, improvement_advice: advice * repeat)
+    value = Value.create!(value: 0.23, stamp: Time.now, metric: Metric.first, improvement_advice: advice * repeat)
     assert_equal advice.length*repeat, value.reload.improvement_advice.length
   end
 
