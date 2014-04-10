69155cb2e833793953d44db9e99749b448ba0f6d
prilop <Michael.Prilop@gmail.com>
2014-04-10 18:16:01 +0200

    added all Data Quality Dimensions from DA as default Dimensions

---
+++ app/models/default_dimension.rb
@@ -0,0 +1,20 @@
+class DefaultDimension < OpenStruct
+
+  def self.all
+    @dimensions ||= load_dimensions
+  end
+
+  def self.find(name)
+    all.select{|d| (d.name == name) }.first
+  end
+
+  private
+
+  def self.load_dimensions
+    dimensions_definition_file = Rails.root.join('config', 'data_quality_dimensions.yml')
+    dimensions_hash = YAML.load_file(dimensions_definition_file)
+    dimension_list = dimensions_hash[:dimensions]
+    dimension_list.map{|d| DefaultDimension.new(d)}
+  end
+
+end
---
+++ test/models/default_dimension_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class DefaultDimensionTest < ActiveSupport::TestCase
+
+  test "DimensionDefault should load dimensions specified in file" do
+    assert DefaultDimension.all.is_a?(Array)
+    assert DefaultDimension.all.first.is_a?(DefaultDimension)
+    assert DefaultDimension.all.first.name.is_a?(String)
+    assert DefaultDimension.all.first.description.is_a?(String)
+  end
+
+  test "DimensionDefault should find Dimension if given name" do
+    availability = DefaultDimension.find('Availability')
+
+    assert availability.is_a?(DefaultDimension)
+    assert_equal 'The degree to which system availability and response times can be guaranteed.', availability.description
+  end
+
+  test "We should have 23 default dimensions to choose from" do
+    assert_equal 23, DefaultDimension.all.count
+  end
+
+end
