bef5f743065ee6a14ccc2d5217fd6457b9ee792d
prilop <Michael.Prilop@gmail.com>
2014-04-11 16:45:40 +0200

    added consequences for subjective assessments

---
+++ test/models/subjective_assessment_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SubjectiveAssessmentTest < ActiveSupport::TestCase
+
+  test "after creating a subjective assessment the dimensions should be available for assessment" do
+    @app = apps(:apps_001)
+    new_assessment = SubjectiveAssessment.new(app: @app)
+    assert_equal @app.dimensions.count, new_assessment.dimensions.count
+  end
+
+  test "a assessment should be stored permanently" do
+    @app = apps(:apps_001)
+    new_assessment = SubjectiveAssessment.new(app: @app)
+    new_assessment.dimensions.first[:satisfactory?] = true
+    assert new_assessment.save
+    assert_equal true, new_assessment.reload.dimensions.first[:satisfactory?]
+  end
+
+end
