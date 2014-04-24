f650e73babb59601a9de3ea94bb215d9a1313108
prilop <Michael.Prilop@gmail.com>
2014-04-24 19:06:22 +0200

    fixed migration chaos

--- db/migrate/20140417155603_rename_subjective_assessment_to_subjective_assessment_survey.rb
+++
@@ -1,5 +0,0 @@
-class RenameSubjectiveAssessmentToSubjectiveAssessmentSurvey < ActiveRecord::Migration
-  def change
-    rename_table :subjective_assessments, :subjective_assessment_surveys
-  end
-end
