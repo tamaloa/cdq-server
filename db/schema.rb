cfad0fc1cb28ad22ac8b50608e624688317a3c02
prilop <Michael.Prilop@gmail.com>
2014-04-28 16:24:53 +0200

    weights for metrics are floats again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140425124559) do
+ActiveRecord::Schema.define(version: 20140428141453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20140425124559) do
 
   create_table "metrics", force: true do |t|
     t.string   "name"
-    t.integer  "weight",       default: 1
+    t.float    "weight",       default: 1.0
     t.integer  "dimension_id"
     t.datetime "created_at"
     t.datetime "updated_at"
