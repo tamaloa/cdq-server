985e576679a20d60da523a82343e5be2337716dc
prilop <Michael.Prilop@gmail.com>
2014-04-30 17:08:39 +0200

    more fixes for rollup calculation of historic data

--- app/models/concerns/calculations.rb
+++ app/models/concerns/calculations.rb
@@ -2,7 +2,7 @@ module Calculations
   extend ActiveSupport::Concern
 
   def weighted_average(input = [])
-    return 0.0 if input.empty?
+    return nil if input.empty?
     weighted_values = input.map{|i| i[:value] * i[:weight]}
     weighted_values.sum / input.map{|i| i[:weight]}.sum
   end
