b2ceb1c8223a6f9a385f26a68afcaa4aa578e1bd
prilop <prilop@localhost>
2014-05-05 15:26:54 +0200

    extracted charting function into concern in preperation for #7687

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -8,6 +8,8 @@ class Dimension < ActiveRecord::Base
   validates_numericality_of :weight, :expectation
 
   include Calculations
+  include Charting
+
 
   after_initialize :set_description
 
@@ -23,17 +25,6 @@ class Dimension < ActiveRecord::Base
   def current_score
     return 0.0 if metrics.empty?
     weighted_average metrics.map{|m| {:value => m.current_value, :weight => m.weight} }
-  end
-
-  def values_for_chart
-    hours_in_one_week = (24*7)
-    # points = []
-    # hours_in_one_week.times do |number|
-    #   points << [ number.hours.ago.to_i*1000, score(number.hours.ago) ]
-    # end
-    # points
-    points = rollups.where(resolution: :hour).order(:stamp)#.limit(hours_in_one_week)
-    points.map{|r| [r.stamp.to_i*1000, r.avg]}
   end
 
   def last_values
--- app/models/metric.rb
+++ app/models/metric.rb
@@ -6,6 +6,8 @@ class Metric < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :dimension_id
 
+  include Charting
+
   def to_s
     name
   end
@@ -28,16 +30,6 @@ class Metric < ActiveRecord::Base
     current_value >= dimension.expectation
   end
 
-  def values_for_chart
-    hours_in_one_week = (24*7)
-    # points = []
-    # hours_in_one_week.times do |number|
-    #   points << [ number.hours.ago.to_i*1000, score(number.hours.ago) ]
-    # end
-    # points
-    points = rollups.where(resolution: :hour).order(:stamp)#.limit(hours_in_one_week)
-    points.map{|r| [r.stamp.to_i*1000, r.avg]}
-  end
 
   def last_values
     values.limit(10)
