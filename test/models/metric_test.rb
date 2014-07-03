e20f29522e8011302725dda532ad3b06ea82c0f0
prilop <Michael.Prilop@gmail.com>
2014-07-03 12:08:00 +0200

    fixed displayed values to be most recent and to honor set filter.

--- test/models/metric_test.rb
+++ test/models/metric_test.rb
@@ -23,6 +23,16 @@ class MetricTest < ActiveSupport::TestCase
     assert @metric.current_value.eql?(0.8)
   end
 
+  test "metric recent_values should return the ten most recent values" do
+    timestamp = Time.now
+    100.times{@metric.record(0.3, timestamp - 10.minutes)}
+    @metric.record(0, timestamp - 5.minutes)
+    @metric.record(0.8, timestamp)
+    assert_equal 10, @metric.recent_values.count
+    assert_equal 0.8, @metric.recent_values.first.value
+    assert_equal 0.0, @metric.recent_values(timestamp - 5.minutes).first.value
+  end
+
   test "metric should only record values between zero and one" do
     refute @metric.record(1.1)
     refute @metric.record(-0.1)
