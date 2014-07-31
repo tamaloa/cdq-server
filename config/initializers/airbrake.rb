6a8080d9952c672c367dc43eeeb44e8fc263b422
prilop <Michael.Prilop@gmail.com>
2014-07-31 10:51:26 +0200

    not ignoring errors any longer (collector is not publicly available, thus no bots to fear?)

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -3,4 +3,16 @@ Airbrake.configure do |config|
   config.host    = 'my-own-airbrake-host.example.com'
   config.port    = 80
   config.secure  = config.port == 443
+  config.ignore_only = []
+
+  ## Ignore Defaults were:
+  # AbstractController::ActionNotFound
+  # ActiveRecord::RecordNotFound
+  # ActionController::RoutingError
+  # ActionController::InvalidAuthenticityToken
+  # ActionController::UnknownAction
+  # ActionController::UnknownHttpMethod
+  # CGI::Session::CookieStore::TamperedWithCookie
+  # Mongoid::Errors::DocumentNotFound
+  # ActionController::UnknownFormat
 end
