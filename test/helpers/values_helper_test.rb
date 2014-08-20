4a0d8978708f3d485d0f4ca6b19b7563accd206e
prilop <Michael.Prilop@gmail.com>
2014-08-20 15:23:38 +0200

    using better visible link colors and small test improvements

--- test/helpers/values_helper_test.rb
+++ test/helpers/values_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
 
   test "unique_links_for_scope should not mutilate urls" do
-    text_containing_links = "<a href='http://ipib.ci.***REMOVED***/companies/richter-werdermann-gerbaulet-hofmann'>richter-werdermann-gerbaulet-hofmann</a> failed Company-dq-test_CO-005.<br>"
+    text_containing_links = Value.find(2).improvement_advice
     text_with_unique_links = unique_links_for_scope(text_containing_links, "bla")
 
     assert text_with_unique_links.match('http://ipib.ci.***REMOVED***/companies/richter-werdermann-gerbaulet-hofmann')
