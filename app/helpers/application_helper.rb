6ba4fd487c17b0f5db9edd7279985db431ec024a
prilop <Michael.Prilop@gmail.com>
2014-08-20 15:16:15 +0200

    generating unique links for each assessment/value to help editors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     return fa_icon("check", class: 'green') if thing.expectation_met?(stamp)
     fa_icon("ban", class: 'red') unless thing.expectation_met?(stamp)
   end
+
+  def unique_links_for_scope(links, scope)
+    links.gsub(/(<a href=')(\S*)('>)/, "\\1\\2##{scope}\\3" )
+  end
 end
--- test/helpers/values_helper_test.rb
+++ test/helpers/values_helper_test.rb
@@ -1,4 +1,11 @@
 require 'test_helper'
 
-class ValuesHelperTest < ActionView::TestCase
+class ApplicationHelperTest < ActionView::TestCase
+
+  test "unique_links_for_scope should not mutilate urls" do
+    text_containing_links = "<a href='http://ipib.ci.moez.fraunhofer.de/companies/richter-werdermann-gerbaulet-hofmann'>richter-werdermann-gerbaulet-hofmann</a> failed Company-dq-test_CO-005.<br>"
+    text_with_unique_links = unique_links_for_scope(text_containing_links, "bla")
+
+    assert text_with_unique_links.match('http://ipib.ci.moez.fraunhofer.de/companies/richter-werdermann-gerbaulet-hofmann')
+  end
 end
