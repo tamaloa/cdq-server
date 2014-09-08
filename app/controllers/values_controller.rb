0538c8cd406b465b2ecfe2847c903f7e348fd4a3
prilop <prilop@localhost>
2014-09-08 15:54:57 +0200

    Removed unneded views and possibilities to manipulate data

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -1,11 +1,5 @@
 class ValuesController < ApplicationController
-  before_action :set_value, only: [:show, :update, :destroy]
-
-  # GET /values
-  # GET /values.json
-  def index
-    @values = Value.all
-  end
+  before_action :set_value, only: [:show]
 
   # GET /values/1
   # GET /values/1.json
@@ -25,30 +19,6 @@ class ValuesController < ApplicationController
         format.html { render action: 'new' }
         format.json { render json: @value.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /values/1
-  # PATCH/PUT /values/1.json
-  def update
-    respond_to do |format|
-      if @value.update(value_params)
-        format.html { redirect_to @value, notice: 'Value was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @value.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /values/1
-  # DELETE /values/1.json
-  def destroy
-    @value.destroy
-    respond_to do |format|
-      format.html { redirect_to values_url }
-      format.json { head :no_content }
     end
   end
 
--- test/controllers/values_controller_test.rb
+++ test/controllers/values_controller_test.rb
@@ -7,13 +7,6 @@ class ValuesControllerTest < ActionController::TestCase
     @value = values(:values_001)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:values)
-  end
-
-
   test "should create value" do
     assert_difference('Value.count') do
       post :create, value: { metric_id: @value.metric_id, stamp: Time.now.utc, value: @value.value,
@@ -30,16 +23,4 @@ class ValuesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should update value" do
-    patch :update, id: @value, value: { metric_id: @value.metric_id, stamp: @value.stamp, value: @value.value }
-    assert_redirected_to value_path(assigns(:value))
-  end
-
-  test "should destroy value" do
-    assert_difference('Value.count', -1) do
-      delete :destroy, id: @value
-    end
-
-    assert_redirected_to values_path
-  end
 end
