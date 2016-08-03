0e251d02acc3dd8f4d622bdd47a9e9377073cb3a
prilop <prilop@localhost>
2016-08-03 16:22:46 +0200

    set branch for normal collector deploys

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,5 +4,7 @@ set :app_sub_domain, application
 set :rails_env, "production"
 set :keep_releases, 5
 
+set :branch, 'default'
+
 # You propably want to do backups before production deploys
 before "deploy:migrate", "db:postgres:dump"
