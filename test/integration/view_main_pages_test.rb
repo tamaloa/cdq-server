7c760e74a9c56e865714470bc9d5b93fe3082d03
prilop <Michael.Prilop@gmail.com>
2017-01-10 11:40:41 +0100

    Add Integration test to check main pages of application for #9832

---
+++ test/integration/view_main_pages_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class ViewMainPagesTest < ActionDispatch::IntegrationTest
+
+  test "viewing and navigating the main pages should work" do
+    get '/'
+    assert_response :success
+    get '/apps/1'
+    assert_response :success
+    get '/dimensions/1'
+    assert_response :success
+    get '/metrics/1'
+    assert_response :success
+    get 'values/1'
+    assert_response :success
+  end
+end
