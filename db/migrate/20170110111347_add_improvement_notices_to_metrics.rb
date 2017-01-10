7107e6cc7ceec74058a32a76c35131550e7eaba6
prilop <Michael.Prilop@gmail.com>
2017-01-10 15:03:40 +0100

    fix migration for #9830

--- db/migrate/20170110111347_add_improvement_notices_to_metrics.rb
+++ db/migrate/20170110111347_add_improvement_notices_to_metrics.rb
@@ -2,7 +2,10 @@ class AddImprovementNoticesToMetrics < ActiveRecord::Migration
   def change
     add_column :metrics, :improvement_advice, :text
     Metric.all.each do |metric|
-      current_improvement_advice = metric.values.last.improvement_advice
+      last_value = metric.values.last
+      next unless last_value
+      current_improvement_advice = last_value.improvement_advice
+      next if current_improvement_advice.blank?
       metric.update(improvement_advice: current_improvement_advice)
     end
   end
