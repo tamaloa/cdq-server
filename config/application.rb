b3166bd6c823077383c693ae536469a3cecae068
prilop <prilop@localhost>
2017-01-10 12:06:53 +0100

    update to rails 4.2 and fix deprecations for #9832

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module DataQualityCollector
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Opt into new test config - avoid deprecation notices
+    config.active_support.test_order = :random
   end
 end
