8b09300c2ef6e502259ffddb3ff38d44ce097b29
Michael <Michael.Prilop@gmail.com>
2017-02-24 15:46:01 +0100

    Remove more secrets

--- test/helpers/values_helper_test.rb
+++ test/helpers/values_helper_test.rb
@@ -6,6 +6,6 @@ class ApplicationHelperTest < ActionView::TestCase
     text_containing_links = Metric.find(2).improvement_advice
     text_with_unique_links = unique_links_for_scope(text_containing_links, "bla")
 
-    assert text_with_unique_links.match('http://ipib.ci.***REMOVED***/companies/richter-werdermann-gerbaulet-hofmann')
+    assert text_with_unique_links.match('http://my-application.example.org/some_url/big-company')
   end
 end
